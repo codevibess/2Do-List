37b72f97429e5ec6494ff64696f3131a62112311
Andriy Cherevko <cherevkin@gmail.com>
2018-04-20 13:27:02 +0200

    Added styles to Main page

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 const port = process.env.port || 8080;
 
 
 
-app.use(express.static(__dirname + 'app'));
-console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
