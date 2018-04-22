dc855c7486e3fb0d05f3e584816d7a9c6673a901
Andriy Cherevko <cherevkin@gmail.com>
2018-04-22 14:07:31 +0200

    added todos page with dummy todos

---
+++ app/controllers/todos.controller.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //show all todos
+    showTodos: (req, res) => {
+        //dummy todos to test
+        const  todos = [
+            {name:"Homework",slug:"homework", description: 'Finish Math'},
+        ];
+        res.render('pages/todos', {todos: todos});
+    }
+};
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('./controllers/main.controllers');
-
+const todosController = require('./controllers/todos.controller');
 
 module.exports = router;
 
-
+//main route
 router.get('/', mainController.showHome);
+
+router.get('/todo', todosController.showTodos);
